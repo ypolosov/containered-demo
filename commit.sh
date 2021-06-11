360c33439f7fb726f60b88ccb68fbea6020a8202
ypolosov <ypolosov@gmail.com>
2021-06-11 18:20:57 +0300

    commit and push to dockerhub

--- commit.sh
+++ commit.sh
@@ -3,3 +3,8 @@ echo -------- COMMIT ---------
 
 source ./variables.sh
 
+set -x
+
+docker commit --author $AUTHOR --message "$MESSAGE" 09515b4b6526 $TAG_NAME
+
+docker push $TAG_NAME
--- variables.sh
+++ variables.sh
@@ -15,3 +15,9 @@ echo "GIT_HASH=$GIT_HASH"
 
 TAG_NAME="$IMAGE_NAME:$GIT_HASH"
 echo "TAG_NAME=$TAG_NAME"
+
+AUTHOR=$(git config user.name):$(git config user.email)
+echo "AUTHOR=$AUTHOR"
+
+MESSAGE=$(git log  --pretty="%s" -n 1)
+echo "MESSAGE=\"$MESSAGE"\"
