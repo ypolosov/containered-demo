4c2da4b390abbbffbf65ee33e93501d971fc89b6
ypolosov <ypolosov@gmail.com>
2021-06-13 12:16:49 +0000

    test

--- commit.sh
+++ commit.sh
@@ -5,6 +5,6 @@ source ./variables.sh
 
 set -x
 
-docker commit --author $AUTHOR --message "$MESSAGE" 09515b4b6526 $TAG_NAME
+docker commit --author $AUTHOR --message "$MESSAGE" containered-demo-360c334 $TAG_NAME
 
 docker push $TAG_NAME
