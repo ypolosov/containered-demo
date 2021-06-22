49f94e034a5a318b1a3e989b4522ed8e94cd9c74
Aleksey Gromovich <grmvch@gmail.com>
2021-06-22 11:53:11 +0000

    fix commit script

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -6,4 +6,4 @@ source ./scripts/container-id.sh
 
 set -x
 
-docker commit --author $AUTHOR --message "$MESSAGE" $CONTAINER_ID $TAG_NAME
+docker commit --author "$AUTHOR" --message "$MESSAGE" $CONTAINER_ID $TAG_NAME
