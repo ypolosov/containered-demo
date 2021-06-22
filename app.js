6c2fc7172b0347747cf94de19e21ff87b8b81ca5
ypolosov <ypolosov@gmail.com>
2021-06-22 07:42:58 +0000

    change test

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 const chalk = require('chalk');
 
-console.log(chalk.yellowBright('Initial project state!!!!!!!1!!!!!!!!222222222!!!!!!!!!!!!!!!!!'));
+console.log(chalk.yellowBright('Initial project state!!!2222!!!!1!!!!!!!!222222222!!!!!!!!!!!!!!!!!'));
